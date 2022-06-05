3c6861303bb6d065cb56da426792a8b8aad7c0ae
VasiliyAMelkozerov <vasiliy.melkozerov@gmail.com>
2022-06-04 19:33:11 -0500

    June 4th checkpoint

--- wrangle.py
+++ wrangle.py
@@ -48,19 +48,33 @@ def drop_nobed_nobath(df):
     df = df[(df.bedroomcnt != 0) & (df.bathroomcnt != 0) & (df.calculatedfinishedsquarefeet >= 70)]
     return df
 
+def squish_outliers(df):
+    #take out properties that are beyond a reasonable price
+    df = df[df.bathroomcnt <= 6]
+    
+    df = df[df.bedroomcnt <= 6]
+
+    df = df[df.taxvaluedollarcnt < 2_000_000]
+
+    return df
+
 def wrangled_zillow(df):
-    df = drop_propertylandusetypeid(df)
+    # df = drop_propertylandusetypeid(df)
 
     df = drop_nulls(df)
 
     df = drop_nobed_nobath(df)
+
+    df = squish_outliers(df)
 
     df.to_csv("zillow.csv", index=False)
 
     return df
 
 def traintestsplit(df):
-    train, test = train_test_split(df, test_size=.2, random_state=123)
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123, stratify=df.fips)
     #stratify is used for categorical data
-    train, validate = train_test_split(train, test_size=.3, random_state=123)
+    train, validate = train_test_split(train_validate, test_size=.3, random_state=123, stratify=train_validate.fips)
+    #I can stratify on county if i want an even distribution of it in my train validate test models
     return train, validate, test
+    #go back and stratify properly on fips
